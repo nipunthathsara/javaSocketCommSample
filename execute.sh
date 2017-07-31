4ad721c0d6637fe72ff2d0ebb11a077ccbf0dec2
nipunthathsara <nipunthathsara@gmail.com>
2017-07-31 11:08:40 +0530

    changes

--- execute.sh
+++ execute.sh
@@ -3,12 +3,13 @@ poolSize=$1
 PORT=$2
 numberOfClients=$3
 
-javac /src/main/java/org/wso2/samples/Server.java
-javac /src/main/java/org/wso2/samples/Client.java
-javac /src/main/java/org/wso2/samples/ClientThread.java
+javac ./src/main/java/org/wso2/samples/ClientThread.java
+javac ./src/main/java/org/wso2/samples/Server.java
+javac ./src/main/java/org/wso2/samples/Client.java
 
-java /src/main/java/org/wso2/samples/Server ${poolSize} ${PORT}
+
+java ./src/main/java/org/wso2/samples/Server ${poolSize} ${PORT}
 for((i=0;i<=${numberOfClients};i++))
 do
-java /src/main/java/org/wso2/samples/Client ${PORT}
+java ./src/main/java/org/wso2/samples/Client ${PORT}
 done
